7a4ed4df45bb4a1c9b4de694d64c7b195fc75e2c
techprempro <shafiksuman0@gmail.com>
2026-09-05 17:44:20 +0600

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+
+android { namespace = "com.techprempro.techprempro"; compileSdk = 35
+    defaultConfig { applicationId = "com.techprempro.techprempro"; minSdk = 24; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+}
